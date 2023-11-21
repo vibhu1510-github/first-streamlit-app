49c765787677ad9f5a876c77be685c9bfa8fe623
vibhu1510-github <151685579+vibhu1510-github@users.noreply.github.com>
2023-11-21 14:01:23 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
-streamlit.text('The contents of the food are still in progress')
-Streamlit.text('empty text')
+streamlit.title('My Parents New Healthy diner')
+
